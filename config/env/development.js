bed380115f5cc7212a72ad6b5abe51f40c7d1691
Arjunsoni111 <er.arjunsoni@gmail.com>
2019-02-14 19:02:04 +0530

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,7 @@
 module.exports = {
  
   db: '',// set db connection   
+  SITE_PATH: '*', // api Site path
   SESSION_SECRET: 'gfFh67fghG8gF7gdjU7w0h5gdT370HF9',
   SESSION_COLLECTION: 'mySession', 
   JWT_CONSTANT: 'JWT_REST_API',
